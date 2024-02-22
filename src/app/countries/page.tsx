b55783c067b1abddd32c52a4c5c5f9828e51eed3
xVitaliy <91687595+xVitaliy@users.noreply.github.com>
2024-02-22 15:34:41 +0200

    fix metadata countries

--- src/app/countries/page.tsx
+++ src/app/countries/page.tsx
@@ -6,35 +6,35 @@ import { CountriesQuery } from '@/graphql/generated';
 import { notFound } from 'next/navigation';
 import { Metadata } from 'next';
 
-// export async function generateMetadata(): Promise<Metadata> {
-//   return {
-//     metadataBase: new URL('https://next14-gold.vercel.app'),
-//     title: 'Countries',
-//     description: 'Countries description',
-//     alternates: {
-//       canonical: '/countries',
-//       languages: {
-//         'uk-UA': '/uk-UA',
-//         'en-US': '/en-US',
-//         'de-DE': '/de-DE',
-//       },
-//     },
-//   };
-// }
-
-export const metadata: Metadata = {
-  metadataBase: new URL('https://next14-gold.vercel.app'),
-  title: 'Countries',
-  description: 'Countries description',
-  alternates: {
-    canonical: '/countries',
-    languages: {
-      'uk-UA': '/uk-UA',
-      'en-US': '/en-US',
-      'de-DE': '/de-DE',
+export async function generateMetadata(): Promise<Metadata> {
+  return {
+    metadataBase: new URL('https://next14-gold.vercel.app'),
+    title: 'Countries',
+    description: 'Countries description',
+    alternates: {
+      canonical: '/countries',
+      languages: {
+        'uk-UA': '/uk-UA',
+        'en-US': '/en-US',
+        'de-DE': '/de-DE',
+      },
     },
-  },
-};
+  };
+}
+
+// export const metadata: Metadata = {
+//   metadataBase: new URL('https://next14-gold.vercel.app'),
+//   title: 'Countries',
+//   description: 'Countries description',
+//   alternates: {
+//     canonical: '/countries',
+//     languages: {
+//       'uk-UA': '/uk-UA',
+//       'en-US': '/en-US',
+//       'de-DE': '/de-DE',
+//     },
+//   },
+// };
 
 async function getData() {
   try {
