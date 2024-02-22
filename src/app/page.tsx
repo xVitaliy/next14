b2436dbc81115986d8bb91ef087f0aaaff355c23
xVitaliy <91687595+xVitaliy@users.noreply.github.com>
2024-02-22 17:58:31 +0200

    fix-

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,58 @@
 import { Box, Button } from '@mui/material';
-import { Metadata } from 'next';
+import { Metadata, ResolvingMetadata } from 'next';
 
-// type Props = {
-//   params: { id: string };
-//   searchParams: { [key: string]: string | string[] | undefined };
-// };
+type Props = {
+  params: { id: string };
+  searchParams: { [key: string]: string | string[] | undefined };
+};
+
+export async function generateMetadata(
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  { params, searchParams }: Props,
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  parent: ResolvingMetadata,
+): Promise<Metadata> {
+  // read route params
+  // const id = params.id;
+
+  // fetch data
+  // const product = await fetch(`https://.../${id}`).then((res) => res.json());
+
+  // optionally access and extend (rather than replace) parent metadata
+  // const previousImages = (await parent).openGraph?.images || [];
+
+  return {
+    metadataBase: new URL('https://next14-gold.vercel.app'),
+    title: 'Дефицит',
+    // description: 'Эта х..та вас ничему не научит',
+    // alternates: {
+    //   canonical: '/default',
+    //   languages: {
+    //     'uk-UA': '/uk-UA',
+    //     'en-US': '/en-US',
+    //     'de-DE': '/de-DE',
+    //   },
+    // },
+    // openGraph: {
+    //   images: [
+    //     {
+    //       url: 'https://upload.wikimedia.org/wikipedia/en/4/41/IMG_Worlds_of_Adventure.png',
+    //     },
+    //   ],
+    //   type: 'website',
+    //   url: 'https://myURL.com',
+    //   title: 'это OG Title',
+    //   description: 'это OG description',
+    //   siteName: 'OG siteName - my TEst project',
+    //   locale: 'uk-UA',
+    // },
+    // robots: {
+    //   index: true,
+    //   follow: true,
+    //   nocache: true,
+    // },
+  };
+}
 
 // export async function generateMetadata(
 //   { params, searchParams }: Props,
@@ -53,37 +101,37 @@ import { Metadata } from 'next';
 //   };
 // }
 
-export const metadata: Metadata = {
-  metadataBase: new URL('https://next14-gold.vercel.app'),
-  title: 'Дефицит',
-  description: 'Тестовый проект с имплементацие континентов и стран',
-  alternates: {
-    canonical: '/',
-    languages: {
-      'uk-UA': '/uk-UA',
-      'en-US': '/en-US',
-      'de-DE': '/de-DE',
-    },
-  },
-  openGraph: {
-    images: [
-      {
-        url: 'https://upload.wikimedia.org/wikipedia/en/4/41/IMG_Worlds_of_Adventure.png',
-      },
-    ],
-    type: 'website',
-    url: 'https://myURL.com',
-    title: 'это OG Title',
-    description: 'это OG description',
-    siteName: 'OG siteName - my TEst project',
-    locale: 'uk-UA',
-  },
-  robots: {
-    index: true,
-    follow: true,
-    nocache: true,
-  },
-};
+// export const metadata: Metadata = {
+//   metadataBase: new URL('https://next14-gold.vercel.app'),
+//   title: 'Дефицит',
+//   description: 'Тестовый проект с имплементацией континентов и стран',
+//   alternates: {
+//     canonical: '/',
+//     languages: {
+//       'uk-UA': '/uk-UA',
+//       'en-US': '/en-US',
+//       'de-DE': '/de-DE',
+//     },
+//   },
+//   openGraph: {
+//     images: [
+//       {
+//         url: 'https://upload.wikimedia.org/wikipedia/en/4/41/IMG_Worlds_of_Adventure.png',
+//       },
+//     ],
+//     type: 'website',
+//     url: 'https://myURL.com',
+//     title: 'это OG Title',
+//     description: 'это OG description',
+//     siteName: 'OG siteName - my TEst project',
+//     locale: 'uk-UA',
+//   },
+//   robots: {
+//     index: true,
+//     follow: true,
+//     nocache: true,
+//   },
+// };
 
 export default function Home() {
   return (
